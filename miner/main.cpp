95cb565d4a466ff60835bbcf0eee7967eebee74a
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-03-13 15:19:41 +0300

    process call

--- miner/main.cpp
+++ miner/main.cpp
@@ -3,14 +3,26 @@
 #include <array>
 #include <functional>
 
+#include "sha256_openssl.h"
 #include  "miner.h"
 
 struct data  {
 
 };
 
-int main() {
+int main() {    
         data data_obj;
 
-        miner(data_obj, []{}, []{});
+        miner miner_obj(
+                data_obj, 
+                [](const data& d, unsigned id){
+                        return crypto::sha256::hash_t{};
+                }, 
+                [] (const crypto::sha256::hash_t& hash ){
+                        return true;
+                }
+        );
+
+        miner_obj.do_work();
+        std::cout << "kekv" << std::endl;
 }
