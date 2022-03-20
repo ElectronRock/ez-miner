191a8533b8246daa925e6a768a2a79bc700aee0b
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-03-20 15:18:15 +0300

    added comparison funciton

--- miner/main.cpp
+++ miner/main.cpp
@@ -10,7 +10,6 @@ using header_t = std::array<unsigned char, 80>;
 
 struct data  {
     header_t header;
-
 };
 
 template<typename T, std::size_t Size>
@@ -23,20 +22,25 @@ auto compute_hash(const std::array<T, Size>& to_compute) {
 int main() {
         data data_obj;
 
-        miner miner_obj(
-                data_obj, 
-                [](const data& d, unsigned id){
-                    header_t to_compute = d.header;
-                    auto* header_ptr = (uint32_t*)to_compute.data();
-                    header_ptr[19] = id;
-
-                    return compute_hash(compute_hash(to_compute));
-                },
-                [] (const crypto::sha256::hash_t& hash ){
-                        return true;
-                }
-        );
-
-        miner_obj.do_work();
-        std::cout << "kekv" << std::endl;
+    constexpr unsigned complexity = 1;
+    miner miner_obj(
+        data_obj, 
+        [](const data& d, unsigned id){
+            header_t to_compute = d.header;
+            auto* header_ptr = (uint32_t*)to_compute.data();
+            header_ptr[19] = id;
+            return compute_hash(compute_hash(to_compute));
+        },
+        [] (const crypto::sha256::hash_t& hash ){
+            uint8_t* presult = (uint8_t * )hash.data();
+            for(unsigned i = 31; i >= 32 - complexity; --i){
+                printf("%02X ", presult[i]);
+                if(presult[i] > 0) return false;
+            }
+            std::cout << std::endl;
+            return true;
+        }
+    );
+    const auto nonce = miner_obj.do_work();
+    std::cout << "Result: " <<  nonce << std::endl;
 }
