1bb95c0e968edaecb5450a1e416ccf13995a21e3
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-03-27 14:56:19 +0300

    code style

--- miner/sha256.h
+++ miner/sha256.h
@@ -7,18 +7,15 @@
 
 namespace crypto {
 
-        class sha256 {
-        public:
-                using hash_t = std::array<unsigned char, 32>;
+    class sha256 {
+    public:
+        using hash_t = std::array<unsigned char, 32>;      
+        virtual ~sha256() = default;
+            
+        virtual void update(const unsigned char* data, std::size_t length) = 0;    
+        virtual void transform(const unsigned char* data) = 0;     
+        virtual const hash_t& finalize() = 0;      
+        virtual const hash_t& hash() = 0;
+    };
 
-                virtual ~sha256() = default;
-                
-                virtual void update(const unsigned char* data, std::size_t length) = 0;
-
-                virtual void transform(const unsigned char* data) = 0;
-
-                virtual const hash_t& finalize() = 0;
-
-                virtual const hash_t& hash() = 0;
-        };
 }
